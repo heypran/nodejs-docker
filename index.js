0c341b5569c891588687a378acb22f880f488c45
Chessy Baweja <baweja.chessy@gmail.com>
2018-12-29 19:14:49 +0530

    added info for docker volume and network

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const HOST = '0.0.0.0';
 
 
 app.get('/',(req,res)=>{
+    console.log("New request!")
     res.status(200).send({success:true,message:"Hi, you docker app is running!"});
 })
 app.listen(PORT,HOST);
